ae45241f9c6d0e8e9fbe0f901b722dce608c7058
Tsvetan Kichuk <toys4babyodua@gmail.com>
2025-02-03 19:41:50 +0100

    Add FitnessEstablishmentViewSet with permissions
    Introduce a new ViewSet for FitnessEstablishment, enabling CRUD operations with appropriate permissions. The queryset uses prefetching for related data and orders establishments by name. This addition improves API functionality and enforces access control.

--- map/views.py
+++ map/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from map.models import FitnessEstablishment
+from map.serializers import FitnessEstablishmentSerializer
+from permissions import IsAdminOrIfAuthenticatedReadOnly
+
+
+class FitnessEstablishmentViewSet(viewsets.ModelViewSet):
+    queryset = FitnessEstablishment.objects.prefetch_related(
+        "additional_services",
+        "sport_activities"
+    ).all().order_by("name")
+
+    serializer_class = FitnessEstablishmentSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
