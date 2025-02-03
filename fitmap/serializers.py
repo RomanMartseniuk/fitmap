22676292cd89269fae2f039602603abba9de01e5
Tsvetan Kichuk <toys4babyodua@gmail.com>
2025-02-03 19:40:08 +0100

    Add serializers for Fitness Establishment models
    Implemented serializers for `FitnessEstablishment`, `SportActivity`, and `AdditionalService` models to facilitate API data representation. Includes both general and detailed serializers to support varied data needs.

---
+++ fitmap/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+
+from fitness_establishments.models import FitnessEstablishment, SportActivity, AdditionalService
+
+
+class FitnessEstablishmentSerializer(serializers.Serializer):
+    class Meta:
+        model = FitnessEstablishment
+        fields = (
+            "name",
+            "price",
+            "address",
+            "working_hours",
+            "working_days",
+            "telephone_number",
+            "email",
+            "sport_activities",
+            "additional_service",
+            "location",
+        )
+
+
+class FitnessEstablishmentDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FitnessEstablishment
+        fields = '__all__'
+
+
+class SportActivitySerializer(serializers.Serializer):
+    class Meta:
+        model = SportActivity
+        fields = ("name", "description")
+
+
+class AdditionalServiceSerializer(serializers.Serializer):
+    class Meta:
+        model = AdditionalService
+        fields = ("name",)
