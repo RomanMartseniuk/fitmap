8c8c0bd9d514e2694384ab3eaca53bb4a4aaa3eb
Tsvetan Kichuk <toys4babyodua@gmail.com>
2025-02-03 19:41:40 +0100

    Add models for FitnessEstablishment, SportActivity, and AdditionalService
    Introduce `FitnessEstablishment` with fields for detailed business info, and establish many-to-many relationships with `SportActivity` and `AdditionalService`. Include definitions for `SportActivity` and `AdditionalService` models to support this structure. This lays the groundwork for managing fitness-related data effectively.

--- map/models.py
+++ map/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class FitnessEstablishment(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    address = models.CharField(max_length=255)
+    working_hours = models.CharField(max_length=255)
+    working_days = models.CharField(max_length=255)
+    telephone_number = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    sport_activities = models.ManyToManyField('SportActivity', blank=True, related_name='activities')
+    additional_services = models.ManyToManyField('AdditionalService', blank=True, related_name='services')
+    location = models.CharField(max_length=255)  # надо посмотреть как прикрутить gps
+
+
+class SportActivity(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class AdditionalService(models.Model):
+    name = models.CharField(max_length=255)
