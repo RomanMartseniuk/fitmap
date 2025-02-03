d00a8957e1a8ba6d8183d2a9b7ddf8969b97f031
Tsvetan Kichuk <toys4babyodua@gmail.com>
2025-02-03 19:43:01 +0100

    Register models in Django admin panel
    Added FitnessEstablishment, SportActivity, and AdditionalService models to the Django admin panel for easier management and visibility. This change facilitates administrative tasks and improves accessibility for these models.

--- map/admin.py
+++ map/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from map.models import FitnessEstablishment, SportActivity, AdditionalService
+
+
+admin.site.register(FitnessEstablishment)
+admin.site.register(SportActivity)
+admin.site.register(AdditionalService)
