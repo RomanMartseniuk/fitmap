cd69f5cbd3c7ea01f072cc55a0c655a594278935
Tsvetan Kichuk <toys4babyodua@gmail.com>
2025-02-04 19:06:32 +0100

    Refactor map/urls.py to remove unused imports and paths.
    Removed the UserLocationView endpoint and its corresponding import as they are no longer in use. Streamlined the code to focus on the FitnessEstablishmentViewSet registration.

--- map/urls.py
+++ map/urls.py
@@ -1,14 +1,9 @@
-from django.urls import path
 from rest_framework import routers
 
 from map.views import FitnessEstablishmentViewSet
-from user.views import UserLocationView
 
 app_name = "map"
 
 router = routers.DefaultRouter()
 router.register("establishments", FitnessEstablishmentViewSet)
-
-urlpatterns = [
-    path("api/users/location/", UserLocationView.as_view(), name="user-location"),
-]
+router.register()
