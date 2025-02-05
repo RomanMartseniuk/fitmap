058d4aecd4cb38c650f525bf4e036240a840e865
Tsvetan Kichuk <toys4babyodua@gmail.com>
2025-02-05 18:13:44 +0100

    Refactor imports and comment out unused geo fields

--- user/serializers.py
+++ user/serializers.py
@@ -1,12 +1,12 @@
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
+# from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 
 class UserSerializer(GeoFeatureModelSerializer):
     class Meta:
         model = get_user_model()
-        geo_field = "location"
+        # geo_field = "location"
         fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
