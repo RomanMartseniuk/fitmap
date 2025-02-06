f29ba0949ac5d3293e16950ec23b16a67b0d2bce
xborismenx <boris.tikhonov.21@gmail.com>
2025-02-07 01:02:43 +0200

    added path gyms/ to GymsNearbyUser view

--- map/urls.py
+++ map/urls.py
@@ -1,11 +1,16 @@
+from django.urls import path
 from rest_framework import routers
 
 
-from map.views import FitnessEstablishmentViewSet
+from map.views import FitnessEstablishmentViewSet, GymsNearbyUser
 
 app_name = "map"
 
 router = routers.DefaultRouter()
 router.register("establishments", FitnessEstablishmentViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("gyms/", GymsNearbyUser.as_view(), name="gyms"),
+]
+
+urlpatterns += router.urls
