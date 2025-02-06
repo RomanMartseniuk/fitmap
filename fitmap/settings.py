8d1a6f211609737b0c621d94fab3f879a687b252
xborismenx <boris.tikhonov.21@gmail.com>
2025-02-07 00:51:06 +0200

    initialized here api key in settings.py

--- fitmap/settings.py
+++ fitmap/settings.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from datetime import timedelta
 from pathlib import Path
+
+HERE_API_KEY = os.getenv("HERE_API_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
