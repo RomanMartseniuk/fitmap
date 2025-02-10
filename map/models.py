6d6402f81cb5e0b16c41fa5f1eba5343d9351f0b
xborismenx <boris.tikhonov.21@gmail.com>
2025-02-10 16:06:01 +0200

    renamed FitnessEstablishment to SportEstablishment & changed fields

--- map/models.py
+++ map/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 
 
-class FitnessEstablishment(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    address = models.CharField(max_length=255)
-    working_hours = models.DateTimeField()
-    working_days = models.DateField()
-    telephone_number = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
-    sport_activities = models.ManyToManyField(
-        "SportActivity", blank=True, related_name="activities"
-    )
-    additional_services = models.ManyToManyField(
-        "AdditionalService", blank=True, related_name="services"
-    )
+class SportEstablishment(models.Model):
+    title = models.CharField(max_length=255)
+    here_id = models.CharField(max_length=255)
+    city = models.ForeignKey("City", on_delete=models.CASCADE)
+    address_label = models.CharField(max_length=255)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    categories = models.ManyToManyField("Category")
+    working_hours = models.DateTimeField(blank=True, null=True)
+    working_days = models.DateField(blank=True, null=True)
+    telephone_number = models.CharField(max_length=255)
+    site = models.URLField(blank=True, null=True)
+    email = models.EmailField(max_length=100)
+    street = models.CharField(max_length=100, blank=True, null=True)
+    house_number = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.name, self.address
