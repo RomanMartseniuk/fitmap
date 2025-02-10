f36aa93a03ac3d65d6ed44e8c48fbd17d8cd2a40
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2025-02-10 12:06:39 +0200

    init develop branch

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import './HomePage.scss';
+
+export const HomePage = () => {
+   return (
+      <>
+         <section className="intro">
+            <div className="intro__container">
+               <h1 className="intro__title">Your personal Sports and Wellness Map</h1>
+            </div>
+         </section>
+      </>
+   );
+};
