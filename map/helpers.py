81ee369b7ee18a52fe9fa07ff4175e0a93d772af
xborismenx <boris.tikhonov.21@gmail.com>
2025-02-15 22:15:27 +0200

    func get_categories, get_contacts moved to helpers.py

---
+++ map/helpers.py
@@ -0,0 +1,31 @@
+from map.models import Category
+
+
+def get_categories(categories: list) -> list[Category]:
+    categories_list = []
+    for category in categories:
+        here_id = category.get("id")
+        name = category.get("name")
+
+        if here_id and name:
+            categories_list.append(Category(here_id=here_id, name=name))
+
+    return categories_list
+
+
+def get_contacts(contacts: list | None) -> tuple | None:
+    if contacts is None:
+        return [], []
+
+    list_phones = []
+    list_sites = []
+    for contact in contacts:
+        phone_contact = contact.get("phone")
+        if phone_contact:
+            list_phones = [number.get("value") for number in phone_contact]
+
+        site_contact = contact.get("www")
+        if site_contact:
+            list_sites = [site_url.get("value") for site_url in site_contact]
+
+    return list_phones, list_sites
