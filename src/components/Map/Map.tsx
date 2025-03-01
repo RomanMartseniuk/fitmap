21a3a8a48c0c5b983e46cfe074c7e2bb940137be
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2025-03-01 22:05:47 +0200

    feat-37 complete v0.2

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,13 +1,60 @@
-import "leaflet/dist/leaflet.css"
-import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, Marker, TileLayer, useMap, ZoomControl } from 'react-leaflet';
 
-export const Map = () => {
+type Coords = [number, number];
+
+type Props = {
+   pos: Coords;
+};
+
+const userIcon = L.icon({
+   iconUrl: '/images/other/icons/blue-user-marker-icon.svg', // Шлях до зображення
+   iconSize: [32, 32], // Розмір іконки
+   iconAnchor: [16, 32], // Точка прив’язки (центр низу іконки)
+   popupAnchor: [0, -32], // Точка відкриття попапа
+});
+
+const ChangeView = ({ center, zoom = 5 }: { center: [number, number]; zoom?: number }) => {
+   const map = useMap();
+
+   useEffect(() => {
+      map.setView(center); // Updates the view when center changes
+      map.setZoom(zoom);
+   }, [center, map, zoom]);
+
+   return null;
+};
+
+export const Map: React.FC<Props> = ({ pos }) => {
+   const defCenter: Coords = [49.014294193038175, 31.186705317899435];
+   const [allowedGeo, setAllowedGeo] = useState(false);
+
+   const [center, setCenter] = useState<[number, number]>(defCenter);
+
+   useEffect(() => {
+      if (pos[1] !== 0 && pos[0] !== 0) {
+         setCenter(pos);
+         setAllowedGeo(true);
+      } else {
+         setCenter(defCenter);
+         setAllowedGeo(false);
+      }
+   }, [pos]);
+
    return (
-      <MapContainer center={[49.23335961878462, 28.470661486706124]} zoom={13}>
+      <MapContainer zoomControl={false}>
+         <ChangeView center={center} zoom={center !== defCenter ? 13 : 5} />
+
          <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
          />
+
+         <ZoomControl position="topright" />
+         
+         {allowedGeo && <Marker position={center} icon={userIcon} />}
       </MapContainer>
    );
 };
--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,15 +1,24 @@
-import styles from "./MapPage.module.scss";
+import styles from './MapPage.module.scss';
 
-import { GymList } from "../../components/GymList";
-import { Map } from "../../components/Map";
-import { SearchForm } from "../../components/SearchForm";
+import { GymList } from '../../components/GymList';
+import { Map } from '../../components/Map';
+import { SearchForm } from '../../components/SearchForm';
+import { useGeolocated } from 'react-geolocated';
 
 export const MapPage = () => {
-   return <div className={styles.mapPage}>
-      <SearchForm className={styles.search} />
-      <div className={styles.container}>
-         <GymList />
-         <Map />
+   const { coords } = useGeolocated({
+      positionOptions: {
+         enableHighAccuracy: false,
+      },
+      userDecisionTimeout: 5000,
+   });
+   return (
+      <div className={styles.mapPage}>
+         <SearchForm className={styles.search} />
+         <div className={styles.container}>
+            <GymList />
+            <Map pos={coords ? [coords?.latitude, coords?.longitude] : [0,0]} />
+         </div>
       </div>
-   </div>;
+   );
 };
