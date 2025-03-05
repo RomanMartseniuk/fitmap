adaf55b02213bb3f709ad9708a1b2550a2c05c14
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2025-03-05 23:00:18 +0200

    feat-37 complete v0.31

--- src/components/GymList/GymList.tsx
+++ src/components/GymList/GymList.tsx
@@ -1,52 +1,34 @@
+import { Gym } from '../../types/Gym';
+import { GymItem } from '../GymItem';
 import PlusIcon from '../PlusIcon/PlusIcon';
 import styles from './GymList.module.scss';
-import styles2 from './GymListItem.module.scss';
 
 
-const item = {
-   id: 0,
-   title: 'Gym',
-   descr: '...',
-   rate: 4.6,
-   price: '200-400₴',
-   category: 'Gym',
+const item: Gym = {
+   id: 1,
+   title: 'Gym Name',
+   description: 'Gym Description',
+   price: 50,
+   rate: 4,
+   category: 'Category',
+   city: 'City',
+   pos: [0, 0],
    img_url: '/images/category_images/gym.jpg',
 };
 
-type Gym = typeof item
 
-type ItemProps = {
-   item: Gym;
-}
-
-const Item: React.FC<ItemProps> = ({item}) => {
-   return (
-      <div className={styles2.item}>
-         <div className={styles2.img}>
-            <img src={item.img_url} alt={item.title} />
-         </div>
-         <h1 className={styles2.title}>{item.title}</h1>
-         <span className={styles2.rate}>
-            <p>{item.rate}</p>
-            <img src="/images/other/icons/black-rate-star-icon.svg" alt="Rate" />
-         </span>
-         <span className={styles2.price}>{item.price}</span>
-         <div className={styles2.button}>
-            <p>See Details</p>
-            <PlusIcon className={styles2.btn_icon} />
-         </div>
-      </div>
-   );
-};
 
 export const GymList = () => {
    return (
-      <div className="gym_list">
-         <nav className="nav">
-            <div className="filters"><img src="" alt="Filters" /></div>
+      <div className={styles.gyms}>
+         <nav className={styles.nav}>
+            <div className={styles.filters}>
+               <p>Filters</p>
+               <img src="/images/other/icons/black-filter-icon.svg" alt="Filters" />
+            </div>
          </nav>
-         <ul className="list">
-            <Item item={item}/>
+         <ul className={styles.list}>
+            <GymItem item={item} />
          </ul>
       </div>
    );
---
+++ src/types/Gym.ts
@@ -0,0 +1,16 @@
+export type Gym = {
+   id: number;
+
+   title: string;
+   description: string;
+
+   price: number;
+   rate: number;
+
+   category: string;
+   city: string;
+
+   pos: [number, number];
+
+   img_url: string;
+};
