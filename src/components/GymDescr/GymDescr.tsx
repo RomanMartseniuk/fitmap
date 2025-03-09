dc8f2dac584489003155316aa9c72a361dbc4eb2
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2025-03-09 23:23:00 +0200

    feat-37 complete v0.4

---
+++ src/components/GymDescr/GymDescr.tsx
@@ -0,0 +1,106 @@
+import styles from './GymDescr.module.scss';
+
+export const GymDescr = () => {
+   return (
+      <div className={styles.gym}>
+         <nav className={styles.nav}>
+            <div className={styles.back_btn}></div>
+         </nav>
+         <div className={styles.content}>
+            <div className={styles.img}>
+               <img src="/images/category_images/gym.jpg" alt="Gym Image" />
+            </div>
+            <div className={styles.main_info}>
+               <div>
+                  <h1>Smartass Gym&Soul</h1>
+                  <span>
+                     <p>4,6</p>
+                     <img src="/images/other/icons/black-rate-star-icon.svg" alt="Rate" />
+                  </span>
+               </div>
+               <p className={styles.adress}>
+                  Gradient БЦ, Korolenkivska St, 4, Kyiv, Ukraine, 01033
+               </p>
+            </div>
+            <div className={styles.scheldue}>
+               <div className={styles.day}>
+                  <p>Sunday</p>
+                  <p>8am-8pm</p>
+               </div>
+               <div className={styles.day}>
+                  <p>Monday</p>
+                  <p>7am-10pm</p>
+               </div>
+               <div className={styles.day}>
+                  <p>Tuesday</p>
+                  <p>7am-10pm</p>
+               </div>
+               <div className={styles.day}>
+                  <p>Wednesday</p>
+                  <p>7am-10pm</p>
+               </div>
+               <div className={styles.day}>
+                  <p>Thursday</p>
+                  <p>7am-10pm</p>
+               </div>
+               <div className={styles.day}>
+                  <p>Friday</p>
+                  <p>7am-10pm</p>
+               </div>
+               <div className={styles.day}>
+                  <p>Saturday</p>
+                  <p>8am-8pm</p>
+               </div>
+            </div>
+            <div className={styles.contact_btn}>
+               <img src="/images/other/icons/black-plus-icon.svg" alt="plus icon" />
+               <p>Contact Gym</p>
+               <img src="/images/other/icons/black-plus-icon.svg" alt="plus icon" />
+            </div>
+            <div className={styles.filters}>
+               <h2>Filter Matches</h2>
+               <ul>
+                  <li>Group classes</li>
+                  <li>Bath</li>
+                  <li>Parking</li>
+               </ul>
+            </div>
+            <div className={styles.offers}>
+               <h2>Offers</h2>
+               <ul>
+                  <li>Gym</li>
+                  <li>Pilates</li>
+                  <li>Run</li>
+                  <li>Cycle</li>
+               </ul>
+            </div>
+            <div className={styles.contacts}>
+               <h2>Contact</h2>
+               <ul>
+                  <li>
+                     <a href="smartass.com">
+                        <img src="" alt="Web" />
+                        <p>smartass.com</p>
+                     </a>
+                  </li>
+                  <li>
+                     <a href="mailto:smartass.com">
+                        <img src="" alt="Email" />
+                        <p>smartass@gmail.com</p>
+                     </a>
+                  </li>
+                  <li>
+                     <a href="tel:smartass.com">
+                        <img src="mailto:smartass.com" alt="Tel" />
+                        <p>smartass.com</p>
+                     </a>
+                  </li>
+               </ul>
+            </div>
+            <div className={styles.reviews}>
+               <h2>Reviews</h2>
+            </div>
+         </div>
+      </div>
+   );
+};
--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -4,6 +4,7 @@ import { GymList } from '../../components/GymList';
 import { Map } from '../../components/Map';
 import { SearchForm } from '../../components/SearchForm';
 import { useGeolocated } from 'react-geolocated';
+import { Outlet } from 'react-router-dom';
 
 export const MapPage = () => {
    const { coords } = useGeolocated({
@@ -16,7 +17,7 @@ export const MapPage = () => {
       <div className={styles.mapPage}>
          <SearchForm className={styles.search} />
          <div className={styles.container}>
-            <GymList />
+            <Outlet />
             <Map pos={coords ? [coords?.latitude, coords?.longitude] : [0,0]} />
          </div>
       </div>
