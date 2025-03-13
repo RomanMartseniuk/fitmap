360b6172420d63e1fa46bc76bd48df4f0f509793
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2025-03-13 22:13:44 +0200

    feat-40 complete v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import './App.scss';
 
 export const App = () => {
-  return (
-    <>
-      <Header />
-      <main className='main'>
-        <Outlet />
-      </main>
-      <Footer />
-    </>
-  );
+   return (
+      <>
+         <Header />
+         <main className="main">
+            <Outlet />
+         </main>
+         <Footer />
+      </>
+   );
 };
