1866395b9aa6785803937a4287d83650f44754e6
xborismenx <boris.tikhonov.21@gmail.com>
2025-03-27 01:30:09 +0200

    fixed wrong name view

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from rest_framework import routers
 
-
-from app.views import FitnessEstablishmentViewSet, GymsNearbyUser, SearchableListView, CategoriesListView, \
-    SportPlaceByCityAndCategoryView
+from app.views import (FitnessEstablishmentViewSet, GymsNearbyUser,
+                       SearchableCityListView, CategoriesListView,
+                       SportPlaceByCityAndCategoryView)
 
 app_name = "app"
 
@@ -12,7 +12,7 @@ router.register("establishments", FitnessEstablishmentViewSet)
 
 urlpatterns = [
     path("gyms-nearby/", GymsNearbyUser.as_view(), name="gyms"),
-    path("city/", SearchableListView.as_view(), name="searchable_city"),
+    path("city/", SearchableCityListView.as_view(), name="searchable_city"),
     path("categories/", CategoriesListView.as_view(), name="categories"),
     path("places-map/", SportPlaceByCityAndCategoryView.as_view(), name="map"),
 
