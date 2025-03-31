566056cef4c1902735eb557064d568a5a47b92d9
xborismenx <boris.tikhonov.21@gmail.com>
2025-04-01 01:05:02 +0300

    updated GymsNearbySerializer
    'deleted previously code, changed inheritance to ModelSerializer'

--- app/serializers.py
+++ app/serializers.py
@@ -34,29 +34,16 @@ class GymsByCityRetrieveSerializer(serializers.ModelSerializer):
         fields = ["title", "here_id", "city", "address_label", "coordinates", "categories"]
 
 
-class GymsNearbySerializer(serializers.Serializer):
-    at = serializers.CharField(required=True)
-    r = serializers.IntegerField(required=False, default=10000)
+class GymsNearbySerializer(serializers.ModelSerializer):
+    distance = serializers.SerializerMethodField()
+    categories = CategorySerializer(read_only=True, many=True)
+    def get_distance(self, obj):
+        if hasattr(obj, 'distance'):
+            return int(obj.distance.m)
+        return None
 
-    def validate_at(self, value):
-        value = value.replace(" ", "")
-        latitude, longitude = map(float, value.split(','))
-        if not (-90 <= latitude <= 90) or not (-180 <= longitude <= 180):
-            raise serializers.ValidationError(
-                "Latitude must be between -90 and 90, longitude between -180 and 180"
-            )
-        return value
-
-    def validate_r(self, value):
-        if not isinstance(value, int):
-            try:
-                value = float(value)
-            except:
-                raise serializers.ValidationError("Radius must be integer 0 < r <= 10000")
-        if value <= 0:
-            raise serializers.ValidationError("Radius must be a positive integer.")
-        return value
-
-    def get_is_default_r(self):
-        if "r" in self.initial_data:
-            return self.initial_data["r"], self.fields["r"].default
+    class Meta:
+        model = SportEstablishment
+        fields = ("title", "city", "address_label", "categories", "distance",
+                  "weekly_schedule", "telephone_number", "site", "email",
+                  "street", "house_number", "district")
