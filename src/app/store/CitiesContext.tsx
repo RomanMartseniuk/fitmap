240077ce647a624dd29c5a8e67a544e75712d900
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2025-05-06 10:56:08 +0300

    start adding error messages

--- src/app/store/CitiesContext.tsx
+++ src/app/store/CitiesContext.tsx
@@ -4,20 +4,26 @@ import getCities from '../../api/citiesApi';
 
 interface CitiesContextProps {
    cities: City[];
+   error: string;
 }
 
-export const CitiesContext = createContext<CitiesContextProps>({ cities: [] });
+export const CitiesContext = createContext<CitiesContextProps>({ cities: [], error: '' });
 
 export const CitiesProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
    const [cities, setCities] = useState<City[]>([]);
+   const [error, setError] = useState<string>('');
 
    useEffect(() => {
       getCities()
-         .then((res) => res.json())
+         .then((res) => {
+            if (!res.ok) throw new Error();
+            return res.json();
+         })
          .then((data) => setCities(data as City[]))
-         .catch()
-         .finally();
+         .catch(() => {
+            setError('Can not load cities... Try again later.');
+         });
    }, []);
 
-   return <CitiesContext.Provider value={{ cities }}>{children}</CitiesContext.Provider>;
+   return <CitiesContext.Provider value={{ cities, error }}>{children}</CitiesContext.Provider>;
 };
