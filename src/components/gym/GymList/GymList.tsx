1f8510c606cf7aab73b52d215c26b73a3473e8a7
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2025-05-26 00:46:54 +0300

    add distance menu

--- src/components/gym/GymList/GymList.tsx
+++ src/components/gym/GymList/GymList.tsx
@@ -2,18 +2,70 @@ import { useOutletContext } from 'react-router-dom';
 import { Gym } from '../../../app/types/Gym';
 import { GymItem } from '../GymItem';
 import styles from './GymList.module.scss';
+import { useState } from 'react';
+
+const DistanceFilter = ({
+   distance,
+   setDistance,
+   setOpen
+}: {
+   distance: number;
+   setDistance: (arg: number) => void;
+   setOpen: (arg: boolean) => void;
+}) => {
+   const [localDist, setLocalDist] = useState(distance);
+
+   return (
+      <div className={styles.distance_menu}>
+         <img
+            src="/images/other/icons/white-close-icon.svg"
+            alt="Filters"
+            className={styles.distance_menu_close}
+            onClick={() => setOpen(false)}
+         />
+         <h3>Set distance to search</h3>
+         <input
+            type="range"
+            min={0}
+            max={20}
+            value={localDist}
+            onChange={(e) => setLocalDist(Number(e.target.value))}
+         />
+         <div className={styles.distance_menu_range}>
+            <span>
+               <p>Minimum</p>
+               <div onClick={() => setLocalDist(0)}>0 km</div>
+            </span>
+            <span>
+               <p>Maximum</p>
+               <div onClick={() => setLocalDist(20)}>20 km</div>
+            </span>
+         </div>
+         <button className={styles.distance_menu_button} onClick={e => {
+            e.preventDefault();
+            setDistance(localDist);
+            setOpen(false);
+         }}>Apply</button>
+      </div>
+   );
+};
 
 export const GymList = () => {
    const gyms: Gym[] = useOutletContext();
 
+   const [distance, setDistance] = useState(0);
+
+   const [changingDistance, setChangingDistance] = useState(false);
+
    return (
       <div className={styles.gyms}>
-         {/* <nav className={styles.nav}>
-            <div className={styles.filters}>
-               <p>Filters</p>
+         <nav className={styles.nav}>
+            <div className={styles.distance} onClick={() => setChangingDistance(!changingDistance)}>
+               <p>Distance</p>
                <img src="/images/other/icons/black-filter-icon.svg" alt="Filters" />
             </div>
-         </nav> */}
+            {changingDistance && <DistanceFilter distance={distance} setDistance={setDistance} setOpen={setChangingDistance}/>}
+         </nav>
          <ul className={styles.list}>
             {/* <GymItem item={item} />
             <GymItem item={item} />
